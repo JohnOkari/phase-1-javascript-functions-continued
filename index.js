ed25a120d356724336fc7b723090334d0315f376
JohnOkari <johnnokari@gmail.com>
2022-11-22 16:57:31 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+// function outer(greeting, msg = "It's a fine day to learn") {
+  
+//   const innerFunction = function (name, lang = "Python") {
+//     // 3
+//     return `${greeting}, ${name}! ${msg} ${lang}`; // 4
+//   };
+//   return innerFunction("student", "JavaScript"); // 5
+// }
+
+// outer("Hello");
+
+let wrapAdjective = function (format = "*") {
+  return function (adjective = "special") {
+    return `You are ${format}${adjective}${format}!`;
+  };
+};
